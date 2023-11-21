bb14c15ddea8082e16c308bd948ef7b646cc1cd6
Tazheeb Shamsi <thshamsi@hotmail.com>
2023-11-21 19:02:12 +0530

    🎉 completed!
    All test cases passed..

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-const assert = require('assert');
+const assert = require("assert");
 
 function countBatteriesByHealth(presentCapacities) {
+  const ratedCapacity = 120;
+
+ 
+  let healthy = 0;
+  let exchange = 0;
+  let failed = 0;
+
+  for (const presentCapacity of presentCapacities) {
+    const sohPercentage = (presentCapacity / ratedCapacity) * 100;
+
+    // Classify the battery based on SoH
+    if (sohPercentage > 80 && sohPercentage <= 100) {
+      healthy++;
+    } else if (sohPercentage >= 62 && sohPercentage <= 80) {
+      exchange++;
+    } else {
+      failed++;
+    }
+  }
+
+  // Return an object with counts
   return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+    healthy,
+    exchange,
+    failed,
   };
 }
 
 function testBucketingByHealth() {
-  console.log('Counting batteries by SoH...');
+  console.log("Counting batteries by SoH...");
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
+  const counts = countBatteriesByHealth(presentCapacities);
+  assert.strictEqual(counts["healthy"], 2);
+  assert.strictEqual(counts["exchange"], 3);
+  assert.strictEqual(counts["failed"], 1);
   console.log("Done counting :)");
 }
 
